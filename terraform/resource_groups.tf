fd275d978c018cd58d1a3b3bdd3009edca3ef027
Louis Ruocco <louisruocco1@gmail.com>
2024-04-19 21:14:28 +0100

    added nonna rg

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "dev-rg" {
   name     = "rg-lou-dev-env"
   location = "UK South"
 }
+
+resource "azurerm_resource_group" "nonna-rg" {
+  name = "rg-lou-nonna"
+  location = "UK South"
+}
