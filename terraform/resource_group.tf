6a6595c5177063df0fb1fe8177bd699659f7c6c4
Louis Ruocco <louisruocco1@gmail.com>
2024-06-29 11:27:44 +0100

    created new rg for fitness runbook and removed old resource-group.tf file

--- terraform/resource_group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "dev-rg" {
-  name     = "rg-lou-dev-env"
-  location = "UK South"
-}
--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "nonna-rg" {
   name     = "rg-lou-nonna"
   location = "UK South"
 }
+
+resource "azurerm_resource_group" "fitness-rg" {
+  name     = "rg-lou-fitness"
+  location = "UK South"
+}
