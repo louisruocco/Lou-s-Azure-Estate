eb54d5a80bbba8fb42e3e0ed15535b4286984b36
Louis Ruocco <louisruocco1@gmail.com>
2024-06-29 11:23:50 +0100

    updated runbook names and prepped fitness runbook to be deployed, still needs tweaking

--- terraform/runbooks.tf
+++ terraform/runbooks.tf
@@ -2,7 +2,32 @@ data "local_file" "nonna-script" {
   filename = "./nonna-script.ps1"
 }
 
-resource "azurerm_automation_runbook" "nonna" {
+resource "azurerm_automation_runbook" "rb-nonna" {
+  name                    = "nonnascript"
+  location                = azurerm_resource_group.nonna-rg.location
+  resource_group_name     = azurerm_resource_group.nonna-rg.name
+  automation_account_name = azurerm_automation_account.nonna-autoacc.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Nonna main script"
+  runbook_type            = "PowerShellWorkflow"
+
+  content = data.local_file.nonna-script.content
+}
+
+resource "azurerm_automation_schedule" "nonna-schedule" {
+  name                    = "nonnaschedule"
+  resource_group_name     = azurerm_resource_group.nonna-rg.name
+  automation_account_name = azurerm_automation_account.nonna-autoacc.name
+  frequency               = "Week"
+  interval                = 1
+  timezone                = "Europe/London"
+  start_time              = "2024-05-20T18:08:00+00:00"
+  description             = "Nonna's schedule for the week"
+  week_days               = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+}
+
+resource "azurerm_automation_runbook" "rb-fitness" {
   name                    = "nonnascript"
   location                = azurerm_resource_group.nonna-rg.location
   resource_group_name     = azurerm_resource_group.nonna-rg.name
