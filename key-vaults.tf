e00700cb6c577d2f5077bd309d51dbacb58bcfa5
Louis Ruocco <louisruocco1@gmail.com>
2025-07-15 15:31:36 +0100

    starting afresh

--- key-vaults.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_key_vault" "kv-nonna" {
-  name                = "nonna-kv"
-  location            = azurerm_resource_group.dev-rg.location
-  resource_group_name = azurerm_resource_group.dev-rg.name
-  tenant_id           = var.tenant_id
-  sku_name            = "standard"
-}
--- storage-accounts.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "nonnastoracc" {
-  name                     = "nonnastoracc"
-  resource_group_name      = azurerm_resource_group.nonna-rg.name
-  location                 = azurerm_resource_group.nonna-rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
